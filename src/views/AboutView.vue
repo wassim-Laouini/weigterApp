4a6b6a82649647e46e5b8c36bdfd1ff87b7197b8
wassim laouini <wassimlaouini02@gmail.com>
2022-07-11 14:56:56 +0200

    ...

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 > Order Now !</h1>
+    <div class="input-edit">
+    <h3>Order Number </h3>
+    <input type="textarea" placeholder="Put Your Table Number Please !">
+    <h3>Table number</h3>
+    <input type="text" placeholder="Put Your Orders Number Please !">
   </div>
+  <div class="menu">
+  
+
+
+
+
+  </div>
+   <div class="btn">
+  <button>Send !</button>
+    </div>
+<button class="fixed">Menu</button>
+  </div>
+  
 </template>
+
+<script>
+
+
+</script>
+
+
+<style>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Need Waiter !</h1>
+    <div class="input-edit">
+    <h3>Table Number </h3>
+    <input type="text" placeholder="Put Your Table Number Please !">
+  </div>
+  <div class="btn">
+  <button>Call</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+
+
+
+
+
 </script>
+<style>
+
+
+</style>
